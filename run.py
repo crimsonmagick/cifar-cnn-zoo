e2119293bc4b4807c19392ec2e1c654da30251a7
Welby Seely <welbyseely@gmail.com>
2026-01-03 19:33:55 -0500

    Updated loop, added reqs, run log

--- run.py
+++ run.py
@@ -4,21 +4,43 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision.models as models
-from torch import cuda, max
 from torch.utils.data import DataLoader
 from torch.utils.data import random_split
 from torchvision import datasets, transforms
 from torchvision.models import VGG16_Weights
 
 
-def evaluate(model, loader, criterion, device):
+def evaluate(model, loader, criterion, device, *, prefix='Val'):
     with torch.no_grad():
-        pass
+        total = 0
+        correct = 0
+        running_loss = 0
+        was_training = model.training
+
+        model.eval()
+        for inputs, labels in loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+
+            loss = criterion(outputs, labels)
+            running_loss += loss.item()
+
+            predicted = outputs.argmax(dim=1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            accuracy = 100.0 * correct / total
+        if was_training:
+            model.train()
+
+        avg_loss = running_loss / len(loader)
+        print(f'{prefix}: Loss: {avg_loss:.4f}, Accuracy: {accuracy:.2f}%')
+        return avg_loss, accuracy
 
 
 def main():
     data_dir = './data'
-    batch_size = 32
+    batch_size = 64
+    seed = 12
 
     vgg16 = models.vgg16(weights=VGG16_Weights.IMAGENET1K_V1)
 
@@ -28,18 +50,28 @@ def main():
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 
+    loader_kwargs = {
+        'batch_size': batch_size,
+        'num_workers': 16,
+        'pin_memory':  torch.cuda.is_available(),
+        'persistent_workers': True,
+        'prefetch_factor': 4,
+        'drop_last': False
+    }
+
     train_dataset = datasets.CIFAR10(root=data_dir, train=True,
                                      download=True, transform=transform)
     test_dataset = datasets.CIFAR10(root=data_dir, train=False,
                                     download=True, transform=transform)
 
-    val_size = math.floor(0.90 * len(train_dataset))
+    val_size = math.floor(0.10 * len(train_dataset))
     train_size = len(train_dataset) - val_size
-    train_dataset, val_dataset = random_split(train_dataset, [train_size, val_size])
+    train_dataset, val_dataset = random_split(train_dataset, [train_size, val_size],
+                                              generator=torch.Generator().manual_seed(seed))
 
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    test_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
-    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    train_loader = DataLoader(train_dataset, shuffle=True, **loader_kwargs)
+    test_loader = DataLoader(test_dataset, shuffle=False, **loader_kwargs)
+    val_loader = DataLoader(val_dataset, shuffle=False, **loader_kwargs)
 
     # Freeze all the layers of the pretrained model
     for param in vgg16.parameters():
@@ -54,7 +86,7 @@ def main():
     optimizer = optim.SGD(vgg16.classifier[6].parameters(), lr=0.001, momentum=0.9)
 
     # Training loop
-    device = torch.device("cuda:0" if cuda.is_available() else "cpu")
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     vgg16 = vgg16.to(device)
 
     for epoch in range(10):
@@ -74,13 +106,16 @@ def main():
 
             running_loss += loss.item()
 
-            _, predicted = max(outputs, 1)  # class with highest logit
+            predicted = outputs.argmax(dim=1)   # class with highest logit
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
         avg_loss = running_loss / len(train_loader)
         accuracy = 100.0 * correct / total
-        print(f'Epoch {epoch + 1}, Loss: {avg_loss:.4f}, Accuracy: {accuracy:.2f}%')
+        print(f'Train: Epoch {epoch + 1}, Loss: {avg_loss:.4f}, Accuracy: {accuracy:.2f}%')
+        evaluate(vgg16, val_loader, criterion, device)
+    evaluate(vgg16, test_loader, criterion, device, prefix='Test')
+
 
 if __name__ == '__main__':
     main()
